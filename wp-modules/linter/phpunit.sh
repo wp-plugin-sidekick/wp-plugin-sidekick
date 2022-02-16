f62e83b1a495944dacb0f5f40ee27683f3e9b762
Phil Johnston <phil.johnston@wpengine.com>
2022-02-16 14:32:37 -0500

    Use single equals for comparison for phpunit also

--- wp-modules/linter/phpunit.sh
+++ wp-modules/linter/phpunit.sh
@@ -1,8 +1,8 @@
 while getopts 'p:m:' flag; do
-  case "${flag}" in
-    p) plugindirname=${OPTARG} ;;
-	m) multisite=${OPTARG} ;;
-  esac
+	case "${flag}" in
+		p) plugindirname=${OPTARG} ;;
+		m) multisite=${OPTARG} ;;
+	esac
 done
 
 
@@ -12,14 +12,14 @@ cd ./../../../../;
 # Make sure that packagejson and composer json exist in wp-content.
 if [ ! -f package.json ] || [ ! -f composer.json ]; then
 	cd -;
-    sh hoister.sh;
+	sh hoister.sh;
 	cd ./../../../../;
 fi
 
 # Make sure that node_modules and vendor directories exist in wp-content.
 if [ ! -d node_modules ] || [ ! -d vendor ]; then
 	cd -;
-    sh install.sh;
+	sh install.sh;
 	cd ./../../../../;
 fi
 
@@ -27,7 +27,7 @@ fi
 npx wp-env start;
 
 # Run PHPunit inside wp-env, targeting the plugin in question.
-if [ "$multisite" == "1" ]; then
+if [ "$multisite" = "1" ]; then
 	npx wp-env run phpunit "phpunit -c /var/www/html/wp-content/plugins/wp-plugin-sidekick/wp-modules/phpunit/phpunit.xml.dist /var/www/html/wp-content/plugins/$plugindirname";
 else
 	npx wp-env run phpunit "WP_MULTISITE=1 phpunit -c /var/www/html/wp-content/plugins/wp-plugin-sidekick/wp-modules/phpunit/phpunit.xml.dist /var/www/html/wp-content/plugins/$plugindirname";
