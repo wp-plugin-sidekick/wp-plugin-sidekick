ac0da28e70d5e9e2cbd30631d61368a9804c6c0e
Phil Johnston <phil.johnston@wpengine.com>
2022-03-01 15:46:10 -0500

    Change name of linter directory to wpps-scripts

--- wp-modules/linter/hoister.sh
+++
@@ -1,2 +0,0 @@
-# Hoist package.json and composer.json to the wp-content directory
-cp package.json composer.json .wp-env.json ./../../../../
--- wp-modules/wpps-scripts/phpunit.sh
+++ wp-modules/wpps-scripts/phpunit.sh
@@ -1,26 +1,36 @@
-while getopts 'p:m:' flag; do
+while getopts 'c:p:m:' flag; do
 	case "${flag}" in
+		c) cwdiswppslinter=${OPTARG} ;;
 		p) plugindirname=${OPTARG} ;;
 		m) multisite=${OPTARG} ;;
 	esac
 done
 
+# Get the absolute path to the plugin we want to check.
+if [ "$cwdiswppslinter" = "1" ]; then
+	plugindir="$(dirname $(dirname $(dirname $(dirname $(realpath $0) ) ) ) )/$plugindirname"
+	wpcontentdir="./../../../../"
+	scriptsdir="$(dirname $(realpath $0) ) )"
+else
+	plugindir="$(dirname $(dirname $(realpath $0) ) )"
+	wpcontentdir="$(dirname $(dirname $(dirname $(dirname $(realpath $0) ) ) ) )"
+	scriptsdir="$plugindir/wpps-scripts/"
+fi
 
 #Go to wp-content directory.
-cd ./../../../../;
+cd $wpcontentdir;
 
 # Make sure that packagejson and composer json exist in wp-content.
 if [ ! -f package.json ] || [ ! -f composer.json ]; then
-	cd -;
-	sh hoister.sh;
-	cd ./../../../../;
+	cd $scriptsdir;
+	sh hoister.sh -c $wpcontentdir;
+	cd $wpcontentdir;
 fi
 
-# Make sure that node_modules and vendor directories exist in wp-content.
-if [ ! -d node_modules ] || [ ! -d vendor ]; then
-	cd -;
-	sh install.sh;
-	cd ./../../../../;
+# Make sure that vendor exists in wp-content.
+if [ ! -d vendor ]; then
+	# Run composer install in wp-content
+	composer install;
 fi
 
 # Start wp-env
