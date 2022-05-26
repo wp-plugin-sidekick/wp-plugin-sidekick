fc0a9723ef1b60564aaacc6da425430e99129f6f
Phil Johnston <phil.johnston@wpengine.com>
2022-05-25 22:20:09 -0400

    Re-organize block module boiler

--- wp-modules/module-boilers/module-boilers/blockmodule/blockmodule.php
+++ wp-modules/module-boilers/module-boilers/blockmodule/blockmodule.php
@@ -27,20 +27,20 @@ function enqueue_block() {
 	$module_dir_path = module_dir_path( __FILE__ );
 	$module_dir_url  = module_dir_url( __FILE__ );
 
-	if ( file_exists( $module_dir_path . 'includes/js/build/index.asset.php' ) ) {
-		$dependencies = require $module_dir_path . 'includes/js/build/index.asset.php';
+	if ( file_exists( $module_dir_path . 'js/build/index.asset.php' ) ) {
+		$dependencies = require $module_dir_path . 'js/build/index.asset.php';
 		$dependencies = $dependencies['dependencies'];
 	} else {
 		return;
 	}
 
 	// Include the frontend component so it can render inside Gutenberg.
-	$js_url = $module_dir_url . 'includes/js/build/index.js';
-	$js_ver = filemtime( $module_dir_path . 'includes/js/build/index.js' );
+	$js_url = $module_dir_url . 'js/build/index.js';
+	$js_ver = filemtime( $module_dir_path . 'js/build/index.js' );
 	wp_register_script( 'blockmodulejs', $js_url, $dependencies, $js_ver, true );
 
-	$css_url = $module_dir_url . 'includes/js/build/index.css';
-	$css_ver = filemtime( $module_dir_path . 'includes/js/build/index.css' );
+	$css_url = $module_dir_url . 'js/build/index.css';
+	$css_ver = filemtime( $module_dir_path . 'js/build/index.css' );
 	wp_register_style( 'blockmodulestyle', $css_url, array(), $css_ver );
 
 	// Register the block.
